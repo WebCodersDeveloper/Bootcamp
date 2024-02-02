af710fea27e2a07c7ef358cc4667f8600fc6729f
WebCodersDeveloper <ibrohimahmedov869@gmail.com>
2024-02-02 22:01:54 +0500

    Bootcamp 3.1

--- src/pages/Advertising/Advsidebar.jsx
+++ src/pages/Advertising/Advsidebar.jsx
@@ -1,10 +1,9 @@
 /* eslint-disable react/prop-types */
-import { FaHackerNews } from "react-icons/fa6";
 import { IoIosPhotos } from "react-icons/io";
 import { MdDiscount, MdMenuOpen, MdReviews } from "react-icons/md";
 
 
-export default function Advsidebar({sidebar, setSidebar, stock, banner, reviews, newsletter, handleBanner, handleNewsletter, handleReviews, hanldeStock}) {
+export default function Advsidebar({sidebar, setSidebar, stock, banner, reviews,  handleBanner,  handleReviews, hanldeStock}) {
 
   return (
     <div className={`border ${sidebar ? 'w-64' : 'w-16'} transition-all h-screen flex flex-col items-center justify-center`}>
@@ -17,7 +16,7 @@ export default function Advsidebar({sidebar, setSidebar, stock, banner, reviews,
                 <button onClick={() => hanldeStock()} className={`${stock ? 'bg-[#4094F7] text-white' : 'bg-none'} rounded-md w-full h-10 hover:bg-[#4094F7] flex items-center pl-5 text-lg transition-all hover:text-white`}>{sidebar ? 'Акции' : <MdDiscount />}</button>
                 <button onClick={() => handleBanner()} className={`${banner ? 'bg-[#4094F7] text-white' : 'bg-none'} rounded-md w-full h-10 hover:bg-[#4094F7] flex items-center pl-5 text-lg transition-all hover:text-white`}>{sidebar ? 'Баннеры' : <IoIosPhotos />}</button>
                 <button onClick={() => handleReviews()} className={`${reviews ? 'bg-[#4094F7] text-white' : 'bg-none'} rounded-md w-full h-10 hover:bg-[#4094F7] flex items-center pl-5 text-lg transition-all hover:text-white`}>{sidebar ? 'Отзывы' : <MdReviews />}</button>
-                <button onClick={() => handleNewsletter()} className={`${newsletter ? 'bg-[#4094F7] text-white' : 'bg-none'} rounded-md w-full h-10 hover:bg-[#4094F7] flex items-center pl-5 text-lg transition-all hover:text-white`}>{sidebar ? 'Рассылка' : <FaHackerNews />}</button>
+
             </div>
         </div>
     </div>
